8343b9d9ce76ab1518dea083cf6e2d06b6590f72
RCempron <ronaldcempronasis@gmail.com>
2026-04-09 08:14:37 +0800

    sample

---
+++ src/views/LoginView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+
+const theme = ref('light')
+
+function onClick() {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<template>
+  <v-responsive class="border rounded" max-height="300">
+    <v-app :theme="theme">
+      <v-app-bar class="px-3">
+        <v-spacer />
+
+        <v-btn
+          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          text="Toggle Theme"
+          slim
+          @click="onClick"
+        />
+      </v-app-bar>
+
+      <v-main>
+        <v-container>
+          <h1 class="my-0">Main Content</h1>
+        </v-container>
+      </v-main>
+    </v-app>
+  </v-responsive>
+</template>
