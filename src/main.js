bc0b0aa217d3fed436bb2be9a990d8cc2ff8cad8
RCempron <ronaldcempronasis@gmail.com>
2026-04-10 09:54:30 +0800

    added logo

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 // Vuetify
+import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
@@ -13,6 +14,9 @@ import router from './router'
 const app = createApp(App)
 
 const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+  },
   components,
   directives,
 })
